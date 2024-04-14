bd0ad518b8407060f06a666ba5bac061f6dcfdb2
baek_kunwoo <78731386+Kunwoo-paik@users.noreply.github.com>
2024-04-15 01:44:57 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.c
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 int main(void){
+    
     int h, m;
-    
     scanf("%d %d", &h, &m);
-    
-    m -= 45;
-        
+    m -= 45;     
     if(m < 0){
         m += 60;
         h -= 1;
